40c8698fd5b9be4a3fddd7c82ae4b5f1873d59bf
szvitek <gy.szvitek@gmail.com>
2023-05-10 16:28:31 +0100

    feat(store): delete task

--- src/store.js
+++ src/store.js
@@ -9,6 +9,10 @@ const store = (set) => ({
   ],
   addTask: (title, state) =>
     set((store) => ({ tasks: [...store.tasks, { title, state }] })),
+  deleteTask: (title) =>
+    set((store) => ({
+      tasks: store.tasks.filter((task) => task.title !== title),
+    })),
 });
 
 export const useStore = create(store);
